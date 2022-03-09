7f9a9478dbe7193157415b4c7d174419d5f37195
Pasechnik0173 <85781126+Pasechnik0173@users.noreply.github.com>
2022-03-09 20:22:43 +0400

    realization csv

--- DAL/Entities/CSVProvider.cs
+++ DAL/Entities/CSVProvider.cs
@@ -1,21 +1,40 @@
 ﻿using DAL.Entities.Abstractions;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace DAL.Entities
 {
     public class CSVProvider : IFileProvider
     {
-        public string PathExtension => throw new System.NotImplementedException();
+        public string PathExtension => "csv";
 
         public ICollection<Resource> LoadFromFile(string path)
         {
-            throw new System.NotImplementedException();
+            List<Resource> resources = new List<Resource>();
+            foreach (string line in File.ReadAllLines(path))
+            {
+                var parameters = line.Split(new[] { ';' });
+                var resource = new Resource(
+                    int.Parse(parameters[0]),
+                    parameters[1],
+                    bool.Parse(parameters[2]),
+                    System.DateTime.Parse(parameters[3]));
+                resources.Add(resource);
+            }
+            return resources;
         }
 
         public void UnLoadToFile(string path, Resource[] resources)
         {
-            throw new System.NotImplementedException();
+            using (StreamWriter stream = new StreamWriter(path, false))
+            {
+                foreach (var resource in resources)
+                {
+                    var line = string.Format("{0};{1};{2};{3}", resource.Id, resource.Address, resource.IsOpen, resource.AccessDate);
+                    stream.WriteLine(line);
+                }
+            }
         }
     }
 }
